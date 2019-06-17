dad47f3c6a88a37f352bf7e07ebfc748a95912f2
afang <a_wzfang@163.com>
2019-06-17 17:58:54 +0800

    修改

--- gitdemo/readme.md
+++ gitdemo/readme.md
@@ -2,3 +2,4 @@
 1.学习
 2.哈哈
 
+markdown地址参考(https://www.jianshu.com/p/191d1e21f7ed)
---
+++ gitdemo2/hello.js
@@ -0,0 +1,16 @@
+var hello = "12345"
+var word = "456789"
+
+var name = "afang"
+var age = "20"
+
+var a = "小花"
+var b = "小明"
+
+var c = "小强"
+
+var d = "小张"
+var d = "小王"
+
+
+var f = "小李"
